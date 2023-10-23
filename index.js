56b34dddc711b8cdc0ef0eeb6fdc209f8617c5fa
Shubham <sk4957199@gmail.com>
2023-10-23 22:30:28 +0530

    mainfile

--- index.js
+++ index.js
@@ -16,12 +16,20 @@ databaseConnect();
 // app.get("/", (req, res) => {
 //   res.send("Hello, Worldjhbhj!");
 // });
+// app.use(cors());
+// Enable CORS for requests from 'http://localhost:5173'
+const allowedOrigins = ["http://localhost:5173"];
 const corsOptions = {
-  origin: "http://localhost:5173/", // Replace with your React app's URL
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Pass cookies and HTTP authentication headers with requests
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
 
+// Use the CORS middleware
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
